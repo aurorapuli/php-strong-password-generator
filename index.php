53f0ddbfdeb71c24faa7dd882064fd354ba2749a
aurorapuli <144793745+aurorapuli@users.noreply.github.com>
2024-01-24 15:17:42 +0100

    add: form

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<form>
+    <label for="password">Quanto deve essere lunga la tua password</label>
+    <input type="number" name="number">
+    <input type="submit" value="invio">
+</form>
+
+<?php
+
+$lenghtPassword = $_GET['number'];
+
+
+
+?>
+</body>
+</html>
